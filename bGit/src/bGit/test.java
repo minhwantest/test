d0aa7a85d6683c5deaa749d7ec3b859c4c5e01fc
snow <snow@DESKTOP-B6CUHV6>
2020-05-18 17:03:53 +0900

    third_commit

--- bGit/src/bGit/test.java
+++ bGit/src/bGit/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("안뇽 ");
 		System.out.println("안녕 반가워 난 조원이야");
+		System.out.println("XXXXXXXXXXXXXXXXXXXX");
 	}
 
 }
